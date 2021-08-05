0bab474f33f09c7e9e110dc60c241a595fd92d2f
tspspi <11219971+tspspi@users.noreply.github.com>
2021-08-05 17:57:06 +0200

    Added rudimentary CLI and implemented missing commands

--- host/src/maincli.c
+++ host/src/maincli.c
@@ -1,9 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <string.h>
 
 #include "./i2c.h"
 #include "./piezoboard.h"
+
+static void printfUUID(struct sysUuid* lpUUID) {
+	printf(
+		"%08x-%04x-%04x-%04x-%02x%02x%02x%02x%02x%02x",
+		lpUUID->p1,
+		lpUUID->p2[0],
+		lpUUID->p2[1],
+		lpUUID->p2[2],
+		lpUUID->p3[0],
+		lpUUID->p3[1],
+		lpUUID->p3[2],
+		lpUUID->p3[3],
+		lpUUID->p3[4],
+		lpUUID->p3[5]
+	);
+}
+
+static void printUsage(int argc, char* argv[]) {
+	printf("Usage: %s [OPTIONS] [COMMANDS]\n\n", argv[0]);
+
+	printf("Supported options:\n");
+	printf("\t-port FILENAME\n");
+	printf("\t\tSets the used I2C port (ex.: /dev/iic1)\n");
+	printf("\t-tries NUMBER\n");
+	printf("\t\tSets the number of retries (default 3)\n");
+
+	printf("\nSupported commands:\n");
+
+	printf("\tid\n\t\tIdentifies the board ID and version\n");
+
+	printf("\tgetth\n\t\tGet the current set threshold\n");
+	printf("\tsetth THRESHOLD\n\t\tSet the current threshold (integer)\n");
+
+	printf("\tgettrig\n\t\tGet current trigger mode\n");
+	printf("\tsettrig MODE\n\t\tSets the current trigger mode\n");
+	printf("\t\t0\tPiezo with external veto\n");
+	printf("\t\t1\tOnly piezo disks\n");
+	printf("\t\t2\tOnly external trigger\n");
+	printf("\t\t3\tPiezo or external trigger\n");
+
+	printf("\trst\n\t\tReset the board and erase EEPROM\n");
+	printf("\tcal\n\t\tExecute recalibration for baseline\n");
+
+	printf("\tst\n\t\tStore settings in EEPROM\n");
+}
 
 int main(int argc, char* argv[]) {
 	struct i2cBus* lpBus;
@@ -11,7 +58,50 @@ int main(int argc, char* argv[]) {
 	enum i2cError ei2c;
 	enum piezoboardError e;
 
-	ei2c = i2cConnect(&lpBus, NULL);
+	unsigned long int i;
+
+	char* lpPortName = NULL;
+	unsigned long int dwRetryCount = 3;
+
+	if(argc < 2) {
+		printUsage(argc, argv);
+		return 1;
+	}
+
+	/*
+		Command line parsing.
+
+		This is done in two steps:
+			* First we do check if there is a supplied port (like /dev/)
+			  or a specification for retries. If so we appply these independent
+			  of the position on the commandline
+			* All other commands are then processed in a loop later on.
+	*/
+	for(i = 1; i < argc; i=i+1) {
+		if(strcmp(argv[i], "-port") == 0) {
+			if(argc <= (i+1)) { printf("Missing port name\n"); printUsage(argc, argv); return 1; }
+			lpPortName = argv[i+1];
+			i = i + 1;
+		} else if(strcmp(argv[i], "-tries") == 0) {
+			if(argc <= (i+1)) { printf("Missing number of retries\n"); printUsage(argc, argv); return 1; }
+			if(sscanf(argv[i+1], "%lu", &dwRetryCount) != 1) { printf("Invalid retry count %s\n", argv[i+1]); printUsage(argc, argv); return 1; }
+			i = i + 1;
+		} else if(strcmp(argv[i], "id") == 0) { continue; }
+		else if(strcmp(argv[i], "getth") == 0) { continue; }
+		else if(strcmp(argv[i], "setth") == 0) { i = i + 1; continue; }
+		else if(strcmp(argv[i], "gettrig") == 0) { continue; }
+		else if(strcmp(argv[i], "settrig") == 0) { i = i + 1; continue; }
+		else if(strcmp(argv[i], "rst") == 0) { continue; }
+		else if(strcmp(argv[i], "cal") == 0) { continue; }
+		else if(strcmp(argv[i], "st") == 0) { continue; }
+		else {
+			printf("Unknown command %s\n", argv[i]);
+			printUsage(argc, argv);
+			return 1;
+		}
+	}
+
+	ei2c = i2cConnect(&lpBus, lpPortName);
 	if(ei2c != i2cE_Ok) {
 		printf("%s:%u Failed to connect with I2C device (%u)\n", __FILE__, __LINE__, ei2c);
 		return 1;
@@ -24,8 +114,178 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
+	int r = 0;
+
+	for(i = 1; i < argc; i=i+1) {
+		if(strcmp(argv[i], "-port") == 0) {
+			i = i + 1;
+			continue;
+		} else if(strcmp(argv[i], "-tries") == 0) {
+			i = i + 1;
+			continue;
+		} else if(strcmp(argv[i], "id") == 0) {
+			struct sysUuid lpBoardUUID;
+			uint8_t boardVersion;
+
+			e = lpPzb->vtbl->identify(lpPzb, &lpBoardUUID, &boardVersion);
+			if(e != piezoE_Ok) {
+				printf("%s:%u Failed to query board UUID and version (%u)\n", __FILE__, __LINE__, e);
+				r = 2;
+				break;
+			}
+
+			printf("Board UUID: "); printfUUID(&lpBoardUUID); printf("\n");
+			printf("Board Version: %u\n", boardVersion);
+
+			usleep(100*1000);
+		} else if(strcmp(argv[i], "getth") == 0) {
+			uint8_t currentThreshold;
+
+			e = lpPzb->vtbl->getThreshold(lpPzb, &currentThreshold);
+			if(e != piezoE_Ok) {
+				printf("%s:%u Failed to query current threshold (%u)\n", __FILE__, __LINE__, e);
+				r = 2;
+				break;
+			}
+
+			printf("Current threshold: %u\n", currentThreshold);
+
+			usleep(100*1000);
+		} else if(strcmp(argv[i], "setth") == 0) {
+			unsigned long int readValue;
+			uint8_t nextThreshold;
+
+			if(argc <= (i+1)) {
+				printf("Missing new threshold value\n");
+				printUsage(argc, argv);
+				r = 1;
+				break;
+			}
+			if(sscanf(argv[i+1], "%lu", &readValue) != 1) {
+				printf("Invalid new threshold value %s\n", argv[i+1]);
+				printUsage(argc, argv);
+				r = 1;
+				break;
+			}
+			if(readValue > 255) {
+				printf("Invalid new threshold value %lu\n", readValue);
+				printUsage(argc, argv);
+				r = 1;
+				break;
+			}
+			nextThreshold = (uint8_t)readValue;
+
+			e = lpPzb->vtbl->setThreshold(lpPzb, nextThreshold);
+			if(e != piezoE_Ok) {
+				printf("%s:%u Failed to set threshold to %u (code %u)\n", __FILE__, __LINE__, nextThreshold, e);
+				r = 2;
+				break;
+			}
+
+			printf("Set new threshold value %u\n", nextThreshold);
+			i = i + 1;
+
+			usleep(100*1000);
+		} else if(strcmp(argv[i], "gettrig") == 0) {
+			enum piezoTriggerMode currentTriggerMode;
+
+			e = lpPzb->vtbl->getTriggerMode(lpPzb, &currentTriggerMode);
+			if(e != piezoE_Ok) {
+				printf("%s:%u Failed to query current trigger mode (%u)\n", __FILE__, __LINE__, e);
+				r = 2;
+				break;
+			}
+
+			switch(currentTriggerMode) {
+				case piezoTriggerMode_PiezoVeto:			printf("Trigger mode: Piezo with veto\n"); break;
+				case piezoTriggerMode_PiezoOnly:			printf("Trigger mode: Piezo only\n"); break;
+				case piezoTriggerMode_Capacitive:			printf("Trigger mode: External only\n"); break;
+				case piezoTriggerMode_PiezoOrCapacitive:	printf("Trigger mode: Piezo or external\n"); break;
+				default:									printf("Trigger mode: Unknown\n"); break;
+			}
+
+			usleep(100*1000);
+		} else if(strcmp(argv[i], "settrig") == 0) {
+			unsigned long int readValue;
+			enum piezoTriggerMode newMode;
+
+			if(argc <= (i+1)) {
+				printf("Missing new trigger mode value\n");
+				printUsage(argc, argv);
+				r = 1;
+				break;
+			}
+			if(sscanf(argv[i+1], "%lu", &readValue) != 1) {
+				printf("Invalid new trigger mode value %s\n", argv[i+1]);
+				printUsage(argc, argv);
+				r = 1;
+				break;
+			}
+
+			switch(readValue) {
+				case 0:			newMode = piezoTriggerMode_PiezoVeto; break;
+				case 1:			newMode = piezoTriggerMode_PiezoOnly; break;
+				case 2:			newMode = piezoTriggerMode_Capacitive; break;
+				case 3:			newMode = piezoTriggerMode_PiezoOrCapacitive; break;
+				default:
+				{
+					printf("Unknown trigger mode %lu\n", readValue);
+					printUsage(argc, argv);
+					r = 1;
+					break;
+				}
+			}
+			if(r == 1) { break; }
+
+			e = lpPzb->vtbl->setTriggerMode(lpPzb, newMode);
+			if(e != piezoE_Ok) {
+				printf("%s:%u Failed to set trigger modde to %u (code %u)\n", __FILE__, __LINE__, newMode, e);
+				r = 2;
+				break;
+			}
+
+			printf("Set new trigger mode value %u\n", newMode);
+			i = i + 1;
+
+			usleep(100*1000);
+		} else if(strcmp(argv[i], "rst") == 0) {
+			e = lpPzb->vtbl->reset(lpPzb);
+			if(e != piezoE_Ok) {
+				printf("%s:%u Failed to perform reset (%u)\n", __FILE__, __LINE__, e);
+				r = 2;
+				break;
+			}
+			printf("Performed reset\n");
+
+			usleep(100*1000);
+		} else if(strcmp(argv[i], "cal") == 0) {
+			e = lpPzb->vtbl->recalibrate(lpPzb);
+			if(e != piezoE_Ok) {
+				printf("%s:%u Failed to perform calibration (%u)\n", __FILE__, __LINE__, e);
+				r = 2;
+				break;
+			}
+			printf("Performed recalibration\n");
+
+			usleep(100*1000);
+		} else if(strcmp(argv[i], "st") == 0) {
+			e = lpPzb->vtbl->storeSettings(lpPzb);
+			if(e != piezoE_Ok) {
+				printf("%s:%u Failed to store settings (%u)\n", __FILE__, __LINE__, e);
+				r = 2;
+				break;
+			}
+			printf("Stored settings\n");
+
+			usleep(100*1000);
+		} else {
+			printf("Unknown command %s\n", argv[i]);
+			printUsage(argc, argv);
+			return 1;
+		}
+	}
 
 	lpPzb->vtbl->release(lpPzb);
 	lpBus->vtbl->release(lpBus);
-	return 0;
+	return r;
 }
--- host/src/piezoboard.c
+++ host/src/piezoboard.c
@@ -23,6 +23,7 @@ enum piezoboardImpl_OpCode {
 	opCode_GetTriggerMode					= 0x07,
 	opCode_Reset							= 0x08,
 	opCode_Recalibrate						= 0x09,
+	opCode_StoreSettings					= 0x0A,
 };
 
 struct piezoboardImpl {
@@ -218,62 +219,184 @@ static enum piezoboardError piezoboardImpl__GetThreshold(
 
 	return piezoE_Ok;
 }
+static uint8_t piezoboardImpl__GetTriggerMode__Command[] = { 0xAA, 0x55, 0xAA, 0x55, opCode_GetTriggerMode, 0x00, 0x07 };
 static enum piezoboardError piezoboardImpl__GetTriggerMode(
 	struct piezoboard* lpSelf,
 	enum piezoTriggerMode* lpTriggerMode
 ) {
 	struct piezoboardImpl* lpThis;
+	enum i2cError ei2c;
+	uint8_t bResponse[4+2+1+1];
 
 	if(lpSelf == NULL) { return piezoE_InvalidParam; }
 
 	lpThis = (struct piezoboardImpl*)(lpSelf->lpReserved);
 
-	return piezoE_ImplementationError;
+	/* Write request */
+	ei2c = lpThis->lpBus->vtbl->write(lpThis->lpBus, lpThis->devAddress, piezoboardImpl__GetTriggerMode__Command, sizeof(piezoboardImpl__GetTriggerMode__Command));
+	if(ei2c != i2cE_Ok) {
+		#ifdef DEBUG
+			printf("%s:%u Write failed (%u)\n", __FILE__, __LINE__, ei2c);
+		#endif
+		return piezoE_Failed;
+	}
+
+	responseDelay();
+
+	/* Read response */
+	ei2c = lpThis->lpBus->vtbl->read(lpThis->lpBus, lpThis->devAddress, bResponse, sizeof(bResponse));
+	if(ei2c != i2cE_Ok) {
+		#ifdef DEBUG
+			printf("%s:%u Read failed (%u)\n", __FILE__, __LINE__, ei2c);
+		#endif
+		return piezoE_Failed;
+	}
+
+	/* Validate response and write outputs ... */
+	if((bResponse[0] != 0xAA) || (bResponse[1] != 0x55) || (bResponse[2] != 0xAA) || (bResponse[3] != 0x55) || (bResponse[4] != opCode_GetTriggerMode) || (bResponse[5] != (sizeof(bResponse) - 5))) {
+		#ifdef DEBUG
+			printf("%s:%u Packet format error\n", __FILE__, __LINE__);
+		#endif
+		return piezoE_CommunicationError;
+	}
+
+	/* Verify checksum */
+	{
+		uint8_t chkSum = 0x00;
+		for(unsigned long int i = 4; i < sizeof(bResponse); i=i+1) {
+			chkSum = chkSum ^ bResponse[i];
+		}
+		if(chkSum != 0x00) {
+			#ifdef DEBUG
+				printf("%s:%u Checksum format error\n", __FILE__, __LINE__);
+			#endif
+			return piezoE_ChecksumError;
+		}
+	}
+
+	if(lpTriggerMode != NULL) {
+		switch(bResponse[6]) {
+			case 0x00:	(*lpTriggerMode) = piezoTriggerMode_PiezoVeto; break;
+			case 0x01:	(*lpTriggerMode) = piezoTriggerMode_PiezoOnly; break;
+			case 0x02:	(*lpTriggerMode) = piezoTriggerMode_Capacitive; break;
+			case 0x03:	(*lpTriggerMode) = piezoTriggerMode_PiezoOrCapacitive; break;
+			default:	return piezoE_CommunicationError;
+		}
+	}
+
+	return piezoE_Ok;
 }
 static enum piezoboardError piezoboardImpl__SetTriggerMode(
 	struct piezoboard* lpSelf,
 	enum piezoTriggerMode trigMode
 ) {
 	struct piezoboardImpl* lpThis;
+	enum i2cError ei2c;
+	uint8_t bCommand[8];
+	unsigned long int i;
 
 	if(lpSelf == NULL) { return piezoE_InvalidParam; }
 
 	lpThis = (struct piezoboardImpl*)(lpSelf->lpReserved);
 
-	return piezoE_ImplementationError;
+	bCommand[0] = 0xAA;
+	bCommand[1] = 0x55;
+	bCommand[2] = 0xAA;
+	bCommand[3] = 0x55;
+	bCommand[4] = opCode_SetTriggerMode;
+	bCommand[5] = 0x01;
+
+	switch(trigMode) {
+		case piezoTriggerMode_PiezoVeto:			bCommand[6] = 0x00; break;
+		case piezoTriggerMode_PiezoOnly:			bCommand[6] = 0x01; break;
+		case piezoTriggerMode_Capacitive:			bCommand[6] = 0x02; break;
+		case piezoTriggerMode_PiezoOrCapacitive:	bCommand[6] = 0x03; break;
+		default: return piezoE_InvalidParam;
+	}
+
+	bCommand[7] = 0x00;
+	for(i = 4; i < sizeof(bCommand)-1; i=i+1) {
+		bCommand[7] = bCommand[7] ^ bCommand[i];
+	}
+
+	ei2c = lpThis->lpBus->vtbl->write(lpThis->lpBus, lpThis->devAddress, bCommand, sizeof(bCommand));
+	if(ei2c != i2cE_Ok) {
+		#ifdef DEBUG
+			printf("%s:%u Write failed (%u)\n", __FILE__, __LINE__, ei2c);
+		#endif
+		return piezoE_CommunicationError;
+	}
+
+	return piezoE_Ok;
 }
+static uint8_t piezoboardImpl__Reset__Command[] = { 0xAA, 0x55, 0xAA, 0x55, opCode_Reset, 0x00, 0x08 };
 static enum piezoboardError piezoboardImpl__Reset(
 	struct piezoboard* lpSelf
 ) {
 	struct piezoboardImpl* lpThis;
+	enum i2cError ei2c;
 
 	if(lpSelf == NULL) { return piezoE_InvalidParam; }
 
 	lpThis = (struct piezoboardImpl*)(lpSelf->lpReserved);
 
-	return piezoE_ImplementationError;
+	/* Write request */
+	ei2c = lpThis->lpBus->vtbl->write(lpThis->lpBus, lpThis->devAddress, piezoboardImpl__Reset__Command, sizeof(piezoboardImpl__Reset__Command));
+	if(ei2c != i2cE_Ok) {
+		#ifdef DEBUG
+			printf("%s:%u Write failed (%u)\n", __FILE__, __LINE__, ei2c);
+		#endif
+		return piezoE_Failed;
+	}
+
+	return piezoE_Ok;
 }
+static uint8_t piezoboardImpl__Recalibrate__Command[] = { 0xAA, 0x55, 0xAA, 0x55, opCode_Recalibrate, 0x00, 0x09 };
 static enum piezoboardError piezoboardImpl__Recalibrate(
 	struct piezoboard* lpSelf
 ) {
 	struct piezoboardImpl* lpThis;
+	enum i2cError ei2c;
 
 	if(lpSelf == NULL) { return piezoE_InvalidParam; }
 
 	lpThis = (struct piezoboardImpl*)(lpSelf->lpReserved);
 
-	return piezoE_ImplementationError;
+	/* Write request */
+	ei2c = lpThis->lpBus->vtbl->write(lpThis->lpBus, lpThis->devAddress, piezoboardImpl__Recalibrate__Command, sizeof(piezoboardImpl__Recalibrate__Command));
+	if(ei2c != i2cE_Ok) {
+		#ifdef DEBUG
+			printf("%s:%u Write failed (%u)\n", __FILE__, __LINE__, ei2c);
+		#endif
+		return piezoE_Failed;
+	}
+
+	return piezoE_Ok;
 }
+static uint8_t piezoboardImpl__StoreSettings__Command[] = { 0xAA, 0x55, 0xAA, 0x55, opCode_StoreSettings, 0x00, 0x0A };
 static enum piezoboardError piezoboardImpl__StoreSettings(
 	struct piezoboard* lpSelf
 ) {
 	struct piezoboardImpl* lpThis;
+	enum i2cError ei2c;
 
 	if(lpSelf == NULL) { return piezoE_InvalidParam; }
 
 	lpThis = (struct piezoboardImpl*)(lpSelf->lpReserved);
 
-	return piezoE_ImplementationError;
+	/* Write request */
+	ei2c = lpThis->lpBus->vtbl->write(lpThis->lpBus, lpThis->devAddress, piezoboardImpl__StoreSettings__Command, sizeof(piezoboardImpl__StoreSettings__Command));
+	if(ei2c != i2cE_Ok) {
+		#ifdef DEBUG
+			printf("%s:%u Write failed (%u)\n", __FILE__, __LINE__, ei2c);
+		#endif
+		return piezoE_Failed;
+	}
+
+	/* Wait till EEPROM is written for sure ... */
+	usleep(500*1000);
+
+	return piezoE_Ok;
 }
 
 
